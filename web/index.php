8bd5a6915a4a4886fefb8bdb3cf366e7c2176b90
Letmecraft <thomas.pob@gmail.com>
2015-04-26 20:09:24 -0400

    Fixes

--- web/index.php
+++ web/index.php
@@ -25,7 +25,7 @@ $app->get('/answer/{name}/{answer}', function ($name, $answer) use ($app) {
         ->setSubject('Nuova risposta!')
         ->setFrom(array('taki@martinaeyrenato.com' => 'Taki il cane grande'))
         ->setTo(array('thomas.pob@gmail.com', 'mmaddi@gmail.com'))
-        ->setBody($name . "ha detto" . $answer . "!");
+        ->setBody("<b>".$name . "</b> ha detto <b>" . $answer . "</b>!");
 
     $app['mailer']->send($message);
 
