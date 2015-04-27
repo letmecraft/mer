955f7e53362228d2a2eaf6bcb78779e338098f83
Letmecraft <thomas.pob@gmail.com>
2015-04-26 20:00:32 -0400

    Fixes

--- web/index.php
+++ web/index.php
@@ -21,10 +21,11 @@ $app->get('/answer/{name}/{answer}', function ($name, $answer) use ($app) {
     $request = $app['request'];
 
     $message = \Swift_Message::newInstance()
+        ->setContentType("text/html");
         ->setSubject('Nuova risposta!')
         ->setFrom(array('taki@martinaeyrenato.com' => 'Taki il cane grande'))
         ->setTo(array('thomas.pob@gmail.com', 'mmaddi@gmail.com'))
-        ->setBody("fsdfsd");
+        ->setBody($name + "ha detto" + $answer + "!");
 
     $app['mailer']->send($message);
 
